6a9856961cf87c297c8449d25b37a562b474f1b9
pandulaDW <pandula.official@gmail.com>
2020-02-09 08:56:17 +0530

    Updates

---
+++ precache-manifest.08d0e19bcf36e3c6408b70b74fc3730c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424884864f7b661960cb4707df78b99f",
+    "url": "/react-lights-out/index.html"
+  },
+  {
+    "revision": "2b3c211f7dee850b53ba",
+    "url": "/react-lights-out/static/css/main.4a4ad324.chunk.css"
+  },
+  {
+    "revision": "40d2866b8db70a82e723",
+    "url": "/react-lights-out/static/js/2.e0b26ce7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-lights-out/static/js/2.e0b26ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3c211f7dee850b53ba",
+    "url": "/react-lights-out/static/js/main.32bd9351.chunk.js"
+  },
+  {
+    "revision": "4d660c832961e90d3328",
+    "url": "/react-lights-out/static/js/runtime-main.e3297320.js"
+  },
+  {
+    "revision": "577145b032463fb3f25722ecfb3496eb",
+    "url": "/react-lights-out/static/media/background.577145b0.jpg"
+  }
+]);
